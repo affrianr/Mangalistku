8ab8d3d212adeb222bc36dcbd18a13dd888c10c8
affrianr <affrianr11@gmail.com>
2024-02-05 14:18:43 +0700

    fix: change mal api to jinkan api

--- server/app.js
+++ server/app.js
@@ -1,31 +1,27 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const errorHandler = require('./middlewares/errorHandler')
-const router = require('./routes/index')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const errorHandler = require("./middlewares/errorHandler");
+const router = require("./routes/index");
+const cors = require("cors");
 
+app.use(cors());
 
-app.use(cors())
-
-
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(router);
 
 // app.listen(PORT, () => console.log("Successfuly accessing PORT: " + PORT));
 
 app.listen(PORT, () => {
-    console.log(`Running on port: ${PORT}`)
-})
+  console.log(`Running on port: ${PORT}`);
+});
 // error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-module.exports = app
+module.exports = app;
